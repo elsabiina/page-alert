e06eb42edf3c336ee0eae72e6bbcba63aa6ef5a6
iina <194478834+elsabiina@users.noreply.github.com>
2025-07-28 17:35:10 +0200

    sql: Initial data. For testing

--- auth-service/src/main/resources/data.sql
+++ auth-service/src/main/resources/data.sql
@@ -1,19 +1,21 @@
--- Ensure the 'users' table exists
-CREATE TABLE IF NOT EXISTS "users" (
-                                       id UUID PRIMARY KEY,
-                                       email VARCHAR(255) UNIQUE NOT NULL,
-                                       password VARCHAR(255) NOT NULL,
-                                       role VARCHAR(50) NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+  id UUID PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
--- Insert the user if no existing user with the same id or email exists
-INSERT INTO "users" (id, email, password, role)
-SELECT '223e4567-e89b-12d3-a456-426614174006', 'testuser@test.com',
-       '$2b$12$7hoRZfJrRKD2nIm2vHLs7OBETy.LWenXXMLKf99W8M4PUwO6KB7fu', 'ADMIN'
+INSERT INTO users (id, email, password)
+SELECT 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
+       'test@example.com',
+       '$2a$12$FMVHO88LVWLLeovp0piTF.npMjYuHHPlZOlX0PZtM7hGuOmXyKcmK'
 WHERE NOT EXISTS (
     SELECT 1
-    FROM "users"
-    WHERE id = '223e4567-e89b-12d3-a456-426614174006'
-       OR email = 'testuser@test.com'
+    FROM users
+    WHERE id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'
+       OR email = 'test@example.com'
 );
+-- It might login with:
+-- email: test@example.com
+-- password: secret
+-- https://bcrypt-generator.com/
 
